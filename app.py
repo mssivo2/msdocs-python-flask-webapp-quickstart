151a267f76e1ce336f09c7f4d2f835ff4e9c9f35
mssivo2 <cesar.odl@hotmail.com>
2023-10-13 16:53:05 -0600

    editquestions

--- app.py
+++ app.py
@@ -9,22 +9,29 @@ TWILIO_ACCOUNT_SID = os.environ.get('AC148063f3ed9416660faafcd76e73fdcb')
 TWILIO_AUTH_TOKEN = os.environ.get('1016ca53ebb4d2d8d5f26d1a8bed6508')
 TWILIO_PHONE_NUMBER = os.environ.get('+14155238886')
 
+
+def generate_response(message_body):
+    # Lógica para generar respuestas basadas en el mensaje del cliente
+    if "horario" in message_body.lower():
+        return "Nuestro horario de atención es de lunes a viernes de 9 AM a 6 PM."
+    elif "productos" in message_body.lower():
+        return "Tenemos una variedad de productos de alta calidad. ¿Hay algo específico que estás buscando?"
+    elif "ofertas" in message_body.lower():
+        return "¡Consulta nuestras últimas ofertas en nuestro sitio web!"
+    else:
+        return "Gracias por tu mensaje. Estamos revisando tu consulta y te responderemos pronto."
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    # Obtiene el cuerpo del mensaje y el número de teléfono del remitente
     message_body = request.form['Body']
     sender_phone_number = request.form['From']
     
-    # Tu lógica para procesar el mensaje y generar una respuesta
-    # En este ejemplo, simplemente devolvemos un mensaje de agradecimiento
-    response_message = "¡Gracias por contactar a nuestra tienda de ropa! Estamos procesando tu mensaje."
+    response_message = generate_response(message_body)
     
-    # Crea una respuesta usando Twilio's MessagingResponse
     response = MessagingResponse()
     response.message(response_message)
     
     return str(response)
 
 if __name__ == "__main__":
-    # Ejecuta la aplicación en el puerto 5000
     app.run(debug=True, port=5000)
